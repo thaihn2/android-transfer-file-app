73d471d5e92cb5ad0033d356ba92642d7c228cfa
Thai Ngoc <ngocthaihn2@gmail.com>
2016-09-29 12:26:54 +0700

    sua xong loi truyen file qua lai. Co the demo duoc

--- TRANSFERFILE/app/src/main/java/code/android/ngocthai/transferfile/Common/Utils/FileTransfer.java
+++ TRANSFERFILE/app/src/main/java/code/android/ngocthai/transferfile/Common/Utils/FileTransfer.java
@@ -32,16 +32,16 @@ public class FileTransfer {
      *
      * @param activity activity called this function
      */
-    public static void showChooser(Activity activity) {
-        // Use the GET_CONTENT intent from the utility class
+    public static void showChooser(Activity activity, int request_code) {
+        //---Use the GET_CONTENT intent from the utility class---
         Intent target = FileUtils.createGetContentIntent();
-        // Create the chooser Intent
+        //---Create the chooser Intent---
         Intent intent = Intent.createChooser(
                 target, activity.getString(R.string.chooser_title));
         try {
-            activity.startActivityForResult(intent, ValuesConst.REQUEST_CODE_CHOOSE_FILE);
+            activity.startActivityForResult(intent, request_code);
         } catch (ActivityNotFoundException e) {
-            // The reason for the existence of aFileChooser
+            //---The reason for the existence of aFileChooser---
         }
     }
 
@@ -49,21 +49,21 @@ public class FileTransfer {
      * Send file to server
      *
      * @param file_path file path of file will send
-     * @param socket
-     * @throws IOException
+     * @param socket    socket of transfer file
+     * @throws IOException            exception input or output of read file
      * @throws ClassNotFoundException
      */
-    public static void SendFile(String file_path, Socket socket, final Activity activity) throws IOException, ClassNotFoundException {
+    public static void SendFile(String file_path, Socket socket) throws IOException, ClassNotFoundException {
         //---create with real file path and get all information of file---
         File file = new File(file_path);
         String file_name = file.getName();
         byte[] buf = new byte[1024];
 
-        //---send file name to server and pass to check---
+        //---send file name to server and pass to check in server---
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.flush();
         oos.writeObject(ValuesConst.pass_transfer + "," + file_name);
-
+        //---read file to send to server---
         OutputStream os = socket.getOutputStream();
         BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
         FileInputStream fis = new FileInputStream(file);
@@ -77,16 +77,49 @@ public class FileTransfer {
         }
     }
 
+
+//    public static void SendFile(String file_path, Socket socket, final Activity activity) throws IOException, ClassNotFoundException {
+//        //---create with real file path and get all information of file---
+//        File file = new File(file_path);
+//        String file_name = file.getName();
+//        byte[] buf = new byte[1024];
+//
+//        //---send file name to server and pass to check in server---
+//        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+//        oos.flush();
+//        oos.writeObject(ValuesConst.pass_transfer + "," + file_name);
+//        //---read file to send to server---
+//        OutputStream os = socket.getOutputStream();
+//        BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+//        FileInputStream fis = new FileInputStream(file);
+//
+//        int i;
+//        int byte_count = 1024;
+//        while ((i = fis.read(buf, 0, 1024)) != -1) {
+//            byte_count += 1024;
+//            bos.write(buf, 0, i);
+//            bos.flush();
+//        }
+//        //---response of server
+//        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+//        final String msg = (String) ois.readObject();
+//        activity.runOnUiThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Toast.makeText(activity, "" + msg, Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
+
     /**
      * Receive file from client
      *
-     * @param socket   socket send file
-     * @param activity activity called this function
+     * @param socket socket send file
      * @return file name and result write file
      * @throws IOException            error if IO Exception
      * @throws ClassNotFoundException error not found class
      */
-    public static String ReceiveFile(Socket socket, final Activity activity) throws IOException, ClassNotFoundException {
+    public static String ReceiveFile(Socket socket) throws IOException, ClassNotFoundException {
 
         byte[] b = new byte[1024];
         int length = 0;
@@ -96,6 +129,7 @@ public class FileTransfer {
         //---receive msg fom client---
         ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
         String msg = (String) ois.readObject();
+
         if (!msg.isEmpty()) {
             String[] temp = msg.split(",");
             file_name = temp[1];
@@ -108,8 +142,8 @@ public class FileTransfer {
                     //---receive file and write file---
                     FileOutputStream fos = new FileOutputStream(file);
                     InputStream is = socket.getInputStream();
-                    BufferedInputStream in2 = new BufferedInputStream(is, 1024);
-                    while ((length = in2.read(b, 0, 1024)) != -1) {
+                    BufferedInputStream bis = new BufferedInputStream(is, 1024);
+                    while ((length = bis.read(b, 0, 1024)) != -1) {
                         byte_count += 1024;
                         fos.write(b, 0, length);
                     }
@@ -128,5 +162,38 @@ public class FileTransfer {
         }
         return result + "," + file_name;
     }
+
+//    public static void sendFile(String file_path, Socket socket) throws IOException {
+//        File f = new File(file_path);
+//
+//        long length = f.length();
+//        String name = f.getName();
+//        byte[] bytes = new byte[16 * 1024];
+//
+//        //---send file name to server and pass to check in server---
+//        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+//        oos.flush();
+//        oos.writeObject(ValuesConst.pass_transfer + "," + name);
+//
+//        InputStream is = new FileInputStream(f);
+//        OutputStream os = socket.getOutputStream();
+//
+//        int count;
+//        while ((count = is.read(bytes)) > 0) {
+//            os.write(bytes, 0, count);
+//        }
+//    }
+//
+//    public static void receiveFile(Socket socket) throws IOException, ClassNotFoundException {
+//
+//        //---receive msg fom client---
+//        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+//        String msg = (String) ois.readObject();
+//
+//        String[] temp = msg.split(",");
+//
+//
+//
+//    }
 
 }
